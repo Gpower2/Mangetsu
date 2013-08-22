6749a4576a9de28b6055e9ba8c406319c2616a5e
garslan <garslan@RCCB43.rc.auth.gr>
2013-08-22 14:17:17 +0300

    Initial project

---
+++ Mangetsu/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1253">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo 'test'
+        ?>
+    </body>
+</html>
